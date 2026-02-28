c6aebf6f6a1c356f7e2aa27774ef06641a1fb667
noneclashofclans <rishitmohanty3@gmail.com>
2026-02-28 14:02:47 +0530

    final

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -193,7 +193,7 @@ const LoginPage = () => {
     setErrors({});
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('https://golden-back-pftn.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
